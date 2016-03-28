c31d99e253511da811a8bb0f5a503190046c21f1
YolandaXiaoAiping <aiping.xiao@mail.utoronto.ca>
2016-03-28 10:07:33 -0400

    123

--- app/src/main/java/com/applicationcourse/mobile/graphmaster/UI/Explosion.java
+++ app/src/main/java/com/applicationcourse/mobile/graphmaster/UI/Explosion.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,6 +24,8 @@ public class Explosion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_explosion);
         mExplosionField = ExplosionField.attach2Window(this);
         addListener(findViewById(R.id.root));
